758268e7838708ab3060441b010b8c8758151b2a
Edson Ferrão <nicferrao@gmail.com>
2025-09-05 16:36:10 -0300

    Use animal view for something

--- Assets/Scripts/Animals/AnimalView.cs
+++ Assets/Scripts/Animals/AnimalView.cs
@@ -1,5 +1,14 @@
+using Animals;
 using UnityEngine;
 
 public class AnimalView : MonoBehaviour
 {
+    public AnimalType type;
+    
+    private Animal animal;
+
+    private void Start()
+    {
+        animal = new Animal(type);
+    }
 }
