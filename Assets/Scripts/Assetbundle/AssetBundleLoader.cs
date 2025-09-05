f3bf536569bb5d02f32750d3f1d7ff72ad506a22
Edson Ferrão <nicferrao@gmail.com>
2025-09-05 15:52:30 -0300

    Load the asset bundle

---
+++ Assets/Scripts/Assetbundle/AssetBundleLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace AssetBundle
+{
+    public class AssetBundleLoader
+    {
+        private readonly string bundlePath = "Assets/AssetBundles";
+        IDictionary<string, UnityEngine.AssetBundle> assetBundles = new Dictionary<string, UnityEngine.AssetBundle>();
+
+        public UnityEngine.AssetBundle LoadAssetBundle(string bundleName)
+        {
+            UnityEngine.AssetBundle assetBundle = null;
+            assetBundle = UnityEngine.AssetBundle.LoadFromFile(Path.Combine(bundlePath, bundleName));
+            if (assetBundle == null)
+            {
+                Debug.LogError($"Failed to load AssetBundle: {bundlePath}");
+            }
+            assetBundles.Add(bundleName, assetBundle);
+            return assetBundle;
+        }
+        public UnityEngine.AssetBundle GetAssetbundle(string assetBundleName)
+        {
+            UnityEngine.AssetBundle bundle = null;
+            if (!assetBundles.TryGetValue(assetBundleName, out bundle))
+            {
+                Debug.LogError($"Assetbundle {assetBundleName} not found");
+            }
+            return bundle;
+        }
+    }
+}
