485e7ce94d7913fd45c12b5f7bcd485df29af20b
Edson Ferrão <nicferrao@gmail.com>
2025-09-05 15:47:43 -0300

    Create the assetBundle builder

---
+++ Assets/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace AssetBundle
+{
+    public class AssetBundleBuilder
+    {
+        [MenuItem("Assets/Build AssetBundle")]
+        public static void Build()
+        {
+            BuildAssetBundle("Assets/AssetBundles");    
+        }
+        
+        public static int BuildAssetBundle(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            
+            AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(
+                path, BuildAssetBundleOptions.StrictMode, EditorUserBuildSettings.activeBuildTarget);
+            if (manifest == null)
+            {
+                Debug.LogError("BuildAssetBundle Failed");
+                return -1;
+            }
+            
+            Debug.Log("Build AssetBundle Success");
+
+            return 1;
+        }
+    }
+}
