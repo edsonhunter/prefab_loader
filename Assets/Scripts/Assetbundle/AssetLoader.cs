212e8cc0fa91092e5d661926fd0bed787e693e4d
Edson Ferrão <nicferrao@gmail.com>
2025-09-05 15:52:44 -0300

    Load the asset by name

---
+++ Assets/Scripts/Assetbundle/AssetLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace AssetBundle
+{
+    public class AssetLoader
+    {
+        public UnityEngine.Object LoadAsset(UnityEngine.AssetBundle bundle, string assetName)
+        {
+            UnityEngine.Object asset = null;
+            if (bundle != null)
+            {
+                asset = bundle.LoadAsset(assetName);
+            }
+            return asset;
+        }
+    }
+}
