7d4f5c53c482176434d9e9d9c4b9f5c62b18e3cd
Edson Ferrão <nicferrao@gmail.com>
2025-09-05 15:55:00 -0300

    Load asset on memory and apply on scene

--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -1,8 +1,33 @@
+﻿
+using System;
+using AssetBundle;
 using UnityEngine;
 
 namespace Spawner
 {
     public class Spawner : MonoBehaviour
     {
+        [field: SerializeField] private AnimalView _animalView;
+
+        private GameObject animalPrefab;
+
+        private void Awake()
+        {
+            AssetBundleManager.LoadAssetBundle();
+        }
+        
+        private void LoadPrefab()
+        {
+            if (_animalView != null)
+            {
+                animalPrefab = AssetBundleManager.LoadAssetByName(_animalView.name) as GameObject;
+                
+            }
+        }
+
+        private void OnValidate()
+        {
+            LoadPrefab();
+        }
     }
 }
