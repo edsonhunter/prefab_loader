b16f5e898f189f481d5f8b7cca31e7a07ca355c6
Edson Ferrão <nicferrao@gmail.com>
2025-09-05 15:53:17 -0300

    AssetBundleManager

---
+++ Assets/Scripts/Assetbundle/AssetBundleManager.cs
@@ -0,0 +1,20 @@
+﻿namespace AssetBundle
+{
+    public class AssetBundleManager
+    {
+        private static readonly string bundleName = "cute_animals";
+        
+        private static AssetBundleLoader bundleLoader =  new AssetBundleLoader();
+        private static AssetLoader assetLoader = new AssetLoader();
+
+        public static UnityEngine.AssetBundle LoadAssetBundle()
+        {
+            return bundleLoader.LoadAssetBundle(bundleName);
+        }
+        public static UnityEngine.Object LoadAssetByName(string assetName)
+        {
+            var bundle = bundleLoader.GetAssetbundle(bundleName);
+            return assetLoader.LoadAsset(bundle, assetName);
+        }
+    }
+}
