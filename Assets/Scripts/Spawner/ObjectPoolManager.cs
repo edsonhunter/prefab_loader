36eac9638f131d714fe8d89c29aa082c6361462f
Edson Ferrão <nicferrao@gmail.com>
2025-09-05 21:02:14 -0300

    small fix

--- Assets/Scripts/Spawner/ObjectPoolManager.cs
+++ Assets/Scripts/Spawner/ObjectPoolManager.cs
@@ -9,8 +9,14 @@ namespace Spawner
         private static IDictionary<string, GameObject> pool = new Dictionary<string, GameObject>();
         private static GameObject _currentGo;
         
-        public static GameObject SpawnObject(GameObject go)
+        public static void SpawnObject(GameObject go)
         {
+            if (go == null)
+            {
+                Debug.LogWarning("Spawning null object");
+                return;
+            }
+            
             if (_currentGo != null)
             {
                 _currentGo.SetActive(false);
@@ -24,7 +30,6 @@ namespace Spawner
             }
             
             _currentGo.SetActive(true);
-            return _currentGo;
         }
     }
 }
